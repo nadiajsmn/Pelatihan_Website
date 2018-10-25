cb5b08825b065a7df8487847dc47bbe9770ab03d
unknown <nadiajasmine@mail.ugm.ac.id>
2018-10-25 18:01:53 +0700

    pertemuan3

---
+++ js/pelatihan3.js
@@ -0,0 +1,9 @@
+function ON(){
+    document.getElementById('lampu').src='pic_bulbon.gif';
+}
+function OFF(){
+    document.getElementById('lampu').src='pic_bulboff.gif';
+}
+function Destroy(){
+    document.getElementById('tulisan').style.display='none';
+}
